f2cbd4445b567a5ee3816e02359fee20de26535c
cristiansep <cristiansepulvedamendez@gmail.com>
2021-01-26 11:01:52 -0300

    First commit

---
+++ models/band.js
@@ -0,0 +1,15 @@
+const {v4: uuid4} = require('uuid');
+ 
+
+
+class Band {
+
+    constructor(name) {
+        this.id = uuid4();
+        this.name = name;
+        this.votes = 0;
+    }
+}
+
+
+module.exports = Band;
